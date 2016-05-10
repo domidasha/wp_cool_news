6e46c0ad333121a2d579667a63bfa0d1935415f6
dasha <daria.stukal@gmail.com>
2016-05-10 17:33:52 +0300

    add searchform

--- wp-content/themes/domiBlog/footer.php
+++ wp-content/themes/domiBlog/footer.php
@@ -1,9 +1,11 @@
 <!---wrap4-->
 <div class="wrap3">
 <div class="container">
-  <div class="footer"> &copy; <?php echo date('Y'); ?> All rights reserved.<a href="www.alltemplateneeds.com" target="_blank"> 
+  <div class="footer"> &copy; <?php echo date('Y'); ?> All rights reserved.
+  <a href="www.alltemplateneeds.com" target="_blank">www.alltemplateneeds.com</a> 
   <a href="<?php home_url(); ?>"> <?php echo bloginfo('name'); ?></a><br />
-    <span>Image courtesy .</span><a href="www.photorack.net" target="_blank" class="active"> www.photorack.net</a> </div>
+    <span>Image courtesy .</span><a href="www.photorack.net" target="_blank" class="active"> www.photorack.net</a> 
+   </div>
 <div class="clearing"></div>
 </div>
 </div>
--- wp-content/themes/domiBlog/searchform.php
+++ wp-content/themes/domiBlog/searchform.php
@@ -1,1 +1,6 @@
-<?php
+<div class="controller">
+<form role="search" method="get" class="search-form" action="<?php echo home_url(); ?>">
+<input type="textbox" name="s" class="form-control" id="search-box" value="<?php the_search_query(); ?>">
+<input class="btn btn-primary no-border" type="submit" class="search-submit" value="Search" />
+</form>
+</div>
